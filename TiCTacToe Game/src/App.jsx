5b7e11efc33d509cd2ee6ba9c75d34be528e8823
IahhMoments <isaiahchhay0@gmail.com>
2025-12-20 15:28:06 -0500

    added a gameover screen but havent built the functionality for it

--- TiCTacToe Game/src/App.jsx
+++ TiCTacToe Game/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import GameOver from "./components/GameOver";
 import { WINNING_COMBINATIONS } from "./WinningCombinations";
 
 const initialGameBoard = [
@@ -80,7 +81,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <p>You Won, {winner}!</p>}
+        {winner && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
