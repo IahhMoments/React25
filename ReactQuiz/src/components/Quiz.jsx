7645b6c921c118e9c49ae0ed5597c5434ef46d90
IahhMoments <isaiahchhay0@gmail.com>
2026-01-20 17:06:13 -0500

    fixed QUESTIONS import error
    forgot to cd 1 folder backward

--- ReactQuiz/src/components/Quiz.jsx
+++ ReactQuiz/src/components/Quiz.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import QUESTIONS from "../questions.js";
+import QUESTIONS from "../../questions.js";
 
 export default function Quiz() {
   const [userAnswer, setUserAnswer] = useState([]);
