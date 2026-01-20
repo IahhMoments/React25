c5159500e15b3f4ee8f9b314f9e3fe4071526e88
IahhMoments <isaiahchhay0@gmail.com>
2026-01-20 17:03:17 -0500

    created components folder with two componets started
    Header and Quiz component

---
+++ ReactQuiz/src/components/Quiz.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+
+export default function Quiz() {
+  const [userAnswer, setUserAnswer] = useState([]);
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswer((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  const activeQuestionIndex = userAnswer.length;
+  return (
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer}>
+            <button
+              className="answer"
+              onClick={() => handleSelectAnswer(answer)}
+            >
+              {answer}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
